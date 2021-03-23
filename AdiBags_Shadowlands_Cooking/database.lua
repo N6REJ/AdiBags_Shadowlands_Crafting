cd09aedea82162542b5b8a79cdae4842000caaaf
N6REJ <programming@hallhome.us>
2021-03-23 09:45:19 -0500

    cooking base layout

---
+++ AdiBags_Shadowlands_Cooking/database.lua
@@ -0,0 +1,22 @@
+-- AdiBags_Shadowlands_Crafting - Meat Database
+-- Created by N6REJ character is Bearesquishy - dalaran please credit whenever.
+-- Source on GitHub: https://github.com/N6REJ/AdiBags_Shadowlands_Crafting
+--
+local ADDON_NAME, ADDON_TABLE, addon = ...
+
+-- Create addon table
+N = { }
+ADDON_TABLE.N = N
+
+-- Label to use
+N["FilterTitle"] = "Cooking"
+
+-- Database of items
+N["database"] = {
+	172052,	-- Aethereal Meat
+	172053,	-- Tenebrous Ribs
+	172054,	-- Raw Seraphic Wing
+	172055,	-- Phantasmal Haunch
+	179314,	-- Creeping Crawler Meat
+	179315,	-- Shadowy Shank
+}
---
+++ AdiBags_Shadowlands_Cooking/meat_database.lua
@@ -0,0 +1,22 @@
+-- AdiBags_Shadowlands_Crafting - Meat Database
+-- Created by N6REJ character is Bearesquishy - dalaran please credit whenever.
+-- Source on GitHub: https://github.com/N6REJ/AdiBags_Shadowlands_Crafting
+--
+local ADDON_NAME, ADDON_TABLE, addon = ...
+
+-- Create addon table
+M = { }
+ADDOM_TABLE.M = M
+
+-- Label to use
+M["FilterTitle"] = "Meat"
+
+-- Database of items
+M["database"] = {
+	172052,	-- Aethereal Meat
+	172053,	-- Tenebrous Ribs
+	172054,	-- Raw Seraphic Wing
+	172055,	-- Phantasmal Haunch
+	179314,	-- Creeping Crawler Meat
+	179315,	-- Shadowy Shank
+}
